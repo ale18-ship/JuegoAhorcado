fa8207e6003a4517d9b2a3b6baa326270d9fab05
ale18-ship <acerap@unicartagena.edu.co>
2025-09-05 17:30:51 -0500

    feat(ahorcado): implementación inicial del juego del Ahorcado

---
+++ src/main/java/com/juegoahorcado/ahorcado/JuegoAhorcado.java
@@ -0,0 +1,7 @@
+package com.juegoahorcado.ahorcado;
+
+public class JuegoAhorcado {
+    public static void main(String[] args) {
+        System.out.println("=== JUEGO DEL AHORCADO ===");
+    }
+}
